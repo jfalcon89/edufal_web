89d81f850f4eab850f4193973edbdb8efef0bff6
jfalcon89 <jfalcon89@hotmail.com>
2022-08-20 23:17:10 -0400

    deploy a heroku

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: `localhost`,
-        user: `${process.env.USER}`,
-        password: `${process.env.PASSWORD}`,
-        database: `${process.env.DATABASE}`
+        host: `us-cdbr-east-06.cleardb.net`,
+        user: `b224673f35b6a3`,
+        password: `cbcfaafb`,
+        database: `heroku_2ead04c218ab733`
     }
 }
