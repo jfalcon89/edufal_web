d3ce4fe354e2b014333b7174602f48cae574069c
Jose Miguel Falcon <jfalcon89@hotmail.com>
2023-02-10 16:19:54 -0400

    se agrega estadistica de gastos por mes

--- routes/inicio.js
+++ routes/inicio.js
@@ -22,6 +22,19 @@ router.get('/inicio', async(req, res) => {
         const cantPagosDB = await pool.query("SELECT COUNT(montoPagoServicio) cantPagos FROM pagos_servicios")
         const sueldosTotalesDB = await pool.query("SELECT SUM(sueldoNeto) sueldosTotales FROM pagos_x_empleados")
 
+        const gastosEneroDB = await pool.query("SELECT SUM(gastosEnero) AS gastosEnero FROM(SELECT SUM(sueldoNeto) AS gastosEnero FROM pagos_x_empleados WHERE periodo = 'Enero' UNION ALL SELECT SUM(montoPagoServicio) AS gastosEnero FROM pagos_servicios WHERE mesPagoServicio = 'Enero') AS temp;")
+        const gastosFebreroDB = await pool.query("SELECT SUM(gastosFebrero) AS gastosFebrero FROM(SELECT SUM(sueldoNeto) AS gastosFebrero FROM pagos_x_empleados WHERE periodo = 'Febrero' UNION ALL SELECT SUM(montoPagoServicio) AS gastosFebrero FROM pagos_servicios WHERE mesPagoServicio = 'Febrero') AS temp;")
+        const gastosMarzoDB = await pool.query("SELECT SUM(gastosMarzo) AS gastosMarzo FROM(SELECT SUM(sueldoNeto) AS gastosMarzo FROM pagos_x_empleados WHERE periodo = 'Marzo' UNION ALL SELECT SUM(montoPagoServicio) AS gastosMarzo FROM pagos_servicios WHERE mesPagoServicio = 'Marzo') AS temp;")
+        const gastosAbrilDB = await pool.query("SELECT SUM(gastosAbril) AS gastosAbril FROM(SELECT SUM(sueldoNeto) AS gastosAbril FROM pagos_x_empleados WHERE periodo = 'Abril' UNION ALL SELECT SUM(montoPagoServicio) AS gastosAbril FROM pagos_servicios WHERE mesPagoServicio = 'Abril') AS temp;")
+        const gastosMayoDB = await pool.query("SELECT SUM(gastosMayo) AS gastosMayo FROM(SELECT SUM(sueldoNeto) AS gastosMayo FROM pagos_x_empleados WHERE periodo = 'Mayo' UNION ALL SELECT SUM(montoPagoServicio) AS gastosMayo FROM pagos_servicios WHERE mesPagoServicio = 'Mayo') AS temp;")
+        const gastosJunioDB = await pool.query("SELECT SUM(gastosJunio) AS gastosJunio FROM(SELECT SUM(sueldoNeto) AS gastosJunio FROM pagos_x_empleados WHERE periodo = 'Junio' UNION ALL SELECT SUM(montoPagoServicio) AS gastosJunio FROM pagos_servicios WHERE mesPagoServicio = 'Junio') AS temp;")
+        const gastosJulioDB = await pool.query("SELECT SUM(gastosJulio) AS gastosJulio FROM(SELECT SUM(sueldoNeto) AS gastosJulio FROM pagos_x_empleados WHERE periodo = 'Julio' UNION ALL SELECT SUM(montoPagoServicio) AS gastosJulio FROM pagos_servicios WHERE mesPagoServicio = 'Julio') AS temp;")
+        const gastosAgostoDB = await pool.query("SELECT SUM(gastosAgosto) AS gastosAgosto FROM(SELECT SUM(sueldoNeto) AS gastosAgosto FROM pagos_x_empleados WHERE periodo = 'Agosto' UNION ALL SELECT SUM(montoPagoServicio) AS gastosAgosto FROM pagos_servicios WHERE mesPagoServicio = 'Agosto') AS temp;")
+        const gastosSeptiempreDB = await pool.query("SELECT SUM(gastosSeptiempre) AS gastosSeptiempre FROM(SELECT SUM(sueldoNeto) AS gastosSeptiempre FROM pagos_x_empleados WHERE periodo = 'Septiempre' UNION ALL SELECT SUM(montoPagoServicio) AS gastosSeptiempre FROM pagos_servicios WHERE mesPagoServicio = 'Septiempre') AS temp;")
+        const gastosOctubreDB = await pool.query("SELECT SUM(gastosOctubre) AS gastosOctubre FROM(SELECT SUM(sueldoNeto) AS gastosOctubre FROM pagos_x_empleados WHERE periodo = 'Octubre' UNION ALL SELECT SUM(montoPagoServicio) AS gastosOctubre FROM pagos_servicios WHERE mesPagoServicio = 'Octubre') AS temp;")
+        const gastosNoviembreDB = await pool.query("SELECT SUM(gastosNoviembre) AS gastosNoviembre FROM(SELECT SUM(sueldoNeto) AS gastosNoviembre FROM pagos_x_empleados WHERE periodo = 'Noviembre' UNION ALL SELECT SUM(montoPagoServicio) AS gastosNoviembre FROM pagos_servicios WHERE mesPagoServicio = 'Noviembre') AS temp;")
+        const gastosDiciembreDB = await pool.query("SELECT SUM(gastosDiciembre) AS gastosDiciembre FROM(SELECT SUM(sueldoNeto) AS gastosDiciembre FROM pagos_x_empleados WHERE periodo = 'Diciembre' UNION ALL SELECT SUM(montoPagoServicio) AS gastosDiciembre FROM pagos_servicios WHERE mesPagoServicio = 'Diciembre') AS temp;")
+
         const gastosGenerales = sueldosTotalesDB[0].sueldosTotales + serviciosTotalesDB[0].serviciosTotales
 
         res.render("dashboard", {
@@ -34,6 +47,18 @@ router.get('/inicio', async(req, res) => {
             serviciosTotales: serviciosTotalesDB[0].serviciosTotales,
             sueldosTotales: sueldosTotalesDB[0].sueldosTotales,
             cantPagos: cantPagosDB[0].cantPagos,
+            gastosEnero: gastosEneroDB[0].gastosEnero,
+            gastosFebrero: gastosFebreroDB[0].gastosFebrero,
+            gastosMarzo: gastosMarzoDB[0].gastosMarzo,
+            gastosAbril: gastosAbrilDB[0].gastosAbril,
+            gastosMayo: gastosMayoDB[0].gastosMayo,
+            gastosJunio: gastosJunioDB[0].gastosJunio,
+            gastosJulio: gastosJulioDB[0].gastosJulio,
+            gastosAgosto: gastosAgostoDB[0].gastosAgosto,
+            gastosSeptiempre: gastosSeptiempreDB[0].gastosSeptiempre,
+            gastosOctubre: gastosOctubreDB[0].gastosOctubre,
+            gastosNoviembre: gastosNoviembreDB[0].gastosNoviembre,
+            gastosDiciembre: gastosDiciembreDB[0].gastosDiciembre,
             login: true,
             name: req.session.name
 
